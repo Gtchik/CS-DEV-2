d5ea814c8ff1f2ca9c7a472288a90413adf72cdb
gtchik <gtchik@hotmail.com>
2020-11-30 16:03:20 +0100

    first commit

---
+++ V1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 08:06:36 2020
+
+@author: gtchi
+"""
+
+from Game import *
+game = Game().setWordsFromFile('D:\\Dossier principal\\Documents\\_Ecole\\CS-DEV\\TP2\\Words.txt').start()
+
